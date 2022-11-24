ee5838794a866bc7748628f9a9568be40de386e1
mariopoons <mariopons02@gmail.com>
2022-11-24 10:02:53 +0100

    ejercicio 1 hecho

--- Assets/Scripts/exercise1.cs
+++ Assets/Scripts/exercise1.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class exercise1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string letter;
+
+    private void Start()
     {
-        
+        isVowel(letter);
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool isVowel(string l)
     {
-        
+        if (l == "a" || l == "e" || l == "i" || l == "o" || l == "u")
+        {
+            Debug.Log("This is a vowel");
+            return true;
+
+        }
+        else
+        {
+            Debug.Log("This is not a vowel");
+            return false;
+        }
     }
 }
